01c48a18b500b19b58e551d1eb4d81cfab8eae1f
Vikms95 <vms1995@hotmail.com>
2022-01-30 04:19:01 +0100

    Insert elements with JS through webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("src and dist linked properly")
+import component from "./component";
+
+const divReference = document.getElementById('content');
+divReference.appendChild(component());
+
+/* <img src="../images/header.jpg" alt="general-restaurant"">
+<div>This restaurant is the best for pizza eating while nerding out</div>
+<script src="main.js"></script> */
