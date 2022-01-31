4485465946f531189a13b83d1bd2ce2ea14ae89d
Vikms95 <vms1995@hotmail.com>
2022-01-31 04:15:46 +0100

    Style left side of header

--- notes.md
+++ notes.md
@@ -1,1 +1,3 @@
 <!-- - Remove all child elements of a div before creating the content after pressing a button > Using divReference.textContent = ''; -->
+
+https://visme.co/blog/website-color-schemes/
--- src/appendHeader.js
+++ src/appendHeader.js
@@ -1,8 +1,32 @@
 function appendHeader(){
+    const headerContainer = document.createElement('div');
     const headerDiv = document.createElement('div');
-    headerDiv.textContent = 'Gotham Pizzeria';
-    headerDiv.classList.add('header')
-    return headerDiv;
+    const logoHeaderDiv = document.createElement('div');
+    const officialWebDiv = document.createElement('div');
+    const githubDiv = document.createElement('div');
+
+    headerContainer.classList.add('header-container');
+
+    headerDiv.textContent = 'Gotham Pizza';
+    headerDiv.classList.add('header');
+    headerContainer.appendChild(headerDiv);
+
+    const logo = new Image();
+    logo.classList.add('gotham-logo');
+    logo.src = '../images/gotham-logo.png';
+    logoHeaderDiv.appendChild(logo);
+    logoHeaderDiv.classList.add('logo-header');
+    headerContainer.appendChild(logoHeaderDiv);
+
+    officialWebDiv.textContent = '- Official website -';
+    officialWebDiv.classList.add('official-web');
+    headerDiv.appendChild(officialWebDiv);
+
+    githubDiv.textContent = 'Created by Vikms';
+    githubDiv.classList.add('github-link');
+    headerContainer.appendChild(githubDiv);
+
+    return headerContainer;
 }
 
 export default appendHeader;
