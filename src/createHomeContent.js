7720fef74ccf2611152f61fd4db66bf6377d820e
Vikms95 <vms1995@hotmail.com>
2022-02-01 05:12:49 +0100

    Finish home page structure

--- src/createHomeContent.js
+++ src/createHomeContent.js
@@ -4,23 +4,37 @@ function createHomeContent(){
     const firstRow = document.createElement('div');
     const imgDiv = document.createElement('div');
     const textDiv = document.createElement('div');
+    const secondRow = document.createElement('div');
+    const imgDiv2 = document.createElement('div');
+    const textDiv2 = document.createElement('div');
 
     element.classList.add('home-content');
     element.appendChild(firstRow);
-
+    
+    firstRow.classList.add('first-row')
     const myImage = new Image();
     myImage.src = '../images/general_restaurant.jpg';
-    imgDiv.classList.add('home-content');
+    imgDiv.classList.add('img-div');
     imgDiv.appendChild(myImage);
-    firstRow.classList.add('first-row')
     firstRow.appendChild(imgDiv);
     textDiv.classList.add('text-div')
-    textDiv.textContent = ". I managed to gthing e on the very right side of the website, outside of the container. (only the footer will be fixed, the header will be scroll-able as well)."
+    textDiv.textContent = "Located at Passatge Fabra i Puig, this cave full of magic will deligth your sights while serving the best pizzas among other things in the area - Located at Passatge Fabra i Puig, this cave full of magic will deligth your sights while serving the best pizzas among other things in the area - Located at Passatge Fabra i Puig, this cave full of magic will deligth your sights while serving the best pizzas among other things in the area";
     firstRow.appendChild(textDiv);
+
+    element.appendChild(secondRow);
+    secondRow.classList.add('second-row')
+
+    textDiv2.classList.add('text-div')
+    textDiv2.textContent = "Located at Passatge Fabra i Puig, this cave full of magic will deligth your sights while serving the best pizzas among other things in the area -Located at Passatge Fabra i Puig, this cave full of magic will deligth your sights while serving the best pizzas among other things in the area - Located at Passatge Fabra i Puig, this cave full of magic will deligth your sights while serving the best pizzas among other things in the area";
+    secondRow.appendChild(textDiv2);
+    const myImage2 = new Image();
+    myImage2.src = '../images/general_restaurant.jpg';
+    imgDiv2.classList.add('img-div');
+    imgDiv2.appendChild(myImage2);
     
-    
-    // element.appendChild(imgDiv);
-    
+    secondRow.appendChild(imgDiv2);
+
+
     return element;
 }
 
